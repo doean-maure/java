bc10667bbbc6bfca3e528ec42f4a309105a61251
Doean Francimae Gilyn Maure <gilynmaure@gmail.com>
2026-03-25 04:31:25 +0000

    Task2.java

---
+++ Task2.java
@@ -0,0 +1,13 @@
+public class Task2 {
+
+    public static void main(String[] args) {
+        
+        int num1 = 3110;
+        byte zero = 0;
+        double num2 = 2.0;
+        boolean bool = true;
+    
+        String output = "H" +  num1 + " w" + zero + "rld " + num2 + " " + bool ;
+        System.out.println(output);
+    }
+}
