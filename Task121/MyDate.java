c4d00142a49de34ee38060793576a7dfe7aa3fef
Doean Francimae Gilyn Maure <gilynmaure@gmail.com>
2026-04-27 16:10:40 +0000

    Demonstrate the java protection of encapsulation

---
+++ Task121/MyDate.java
@@ -0,0 +1,15 @@
+package Task121;
+
+public class MyDate {
+    public static void main(String[] args) {
+        DateTask date1 = new DateTask();
+        DateTask date2 = new DateTask();
+        
+        date1.setDate(1, 1, 1978);
+        date2.setDate(9, 21, 1984);
+
+        System.out.println("Date 1: " + date1.toString());
+        System.out.println("Date 2: " + date2.toString());
+        DateTask.leapYears();
+    }
+}
